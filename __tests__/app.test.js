060d44343d7229b52f7307fe0237ac4e4a550446
Derek Hudgins <hudgins.derek@gmail.com>
2021-07-22 16:30:41 -0700

    test created for create

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,8 +3,18 @@ import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
 
-describe('demo routes', () => {
+describe('cat routes', () => {
   beforeEach(() => {
     return setup(pool);
+  });
+
+  it('creates a cat from post', async () => {
+    const garfield = { name: 'garfeild', age: 15, favoriteFood: 'lassagna' };
+    const res = await (await request(app).post('/api/v1/cats')).setEncoding(garfield);
+
+    expect(res.body).toEqual({
+      id: '1',
+      ...garfield
+    });
   });
 });
