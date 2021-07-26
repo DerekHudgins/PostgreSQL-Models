2909020eadf335b85bb478f499a29405275923c9
Derek Hudgins <hudgins.derek@gmail.com>
2021-07-25 19:21:12 -0700

    test passing for get

--- __tests__/movie.test.js
+++ __tests__/movie.test.js
@@ -2,15 +2,18 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
-import Movie from '../lib/models/Movie';
+import Movie from '../lib/models/Movie.js';
 
-describe('movie routes', () => {
+describe('Movie routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
   
-  it('creates a game from POST', async () => {
-    const royal = { title: 'WThe Royal Tenenbaums', director: 'Wes Anderson', genre: 'comedy-drama' };
+  it('creates a movie from POST', async () => {
+    const royal = { 
+      title: 'WThe Royal Tenenbaums', 
+      director: 'Wes Anderson', 
+      genre: 'comedy-drama' };
   
     const res = await request(app)
       .post('/api/v1/movies')
