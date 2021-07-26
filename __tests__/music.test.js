a6cec0eb2ffca2aaab4ee73e687134614c9c294e
Derek Hudgins <hudgins.derek@gmail.com>
2021-07-26 09:10:49 -0700

    test created for getting byId

--- __tests__/music.test.js
+++ __tests__/music.test.js
@@ -2,6 +2,7 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
+import Music from '../lib/models/Music.js';
 
 
 describe('music routes', () => {
@@ -24,5 +25,15 @@ describe('music routes', () => {
       id: '1',
       ...Boy
     });
+  });
+  it('gets a band by id ', async () => {
+    const boy = await Music.insert ({ 
+      artist: 'Boy Harshe', 
+      album: 'Careful', 
+      song: 'fate' 
+    });
+    const res = await request(app).get(`/api/v1/music/${boy.id}`);
+
+    expect(res.body).toEqual(boy);
   });
 });
